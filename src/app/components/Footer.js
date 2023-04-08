01704a61c1d8e5295f9f5b6968cfd254cc3d38a5
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2023-04-08 22:17:39 +0800

    text

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='flex flex-col justify-center items-center gap-4 mt-6 p-6'>
-        <p>Telemetry data from Azure IoT Central via Fetch API</p>
+        <p className='text-center'>Telemetry data from Azure IoT Central via Fetch API</p>
         <p>Device: Raspberry Pi Pico W</p>
         <p>Protocol: MQTT for Device to Cloud</p>
         <a className='font-bold' href='https://github.com/gurpreet2188/picoiot'> Made by Gurpreet Singh, source code: Github</a>
