7450f7f1756f5f7d5657083629a98ddb7f577a46
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2023-04-09 22:08:42 +0800

    command form

--- src/app/components/Command.js
+++ src/app/components/Command.js
@@ -1,55 +1,44 @@
 'use client'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 
 function Command() {
-    const [submit, setSubmit] = useState(false)
     const [pass, setPass] = useState('')
     const [msg, setMsg] = useState('')
-    useEffect(() => {
-        let check = true
-    
-        
-        if (check) {
-          const f = async (command) => {
-            const res = await fetch('/api/command', {
-              method: 'POST',
-              headers: {
+    const [res, setRes] = useState()
+    const formRef = useRef(null)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setRes()
+        const command = { "pass": pass, "msg": { "oled": msg.split(" ") } }
+        const res = await fetch('/api/command', {
+            method: 'POST',
+            headers: {
                 "Content-Type": "application/json",
-              },
-              body: JSON.stringify(command)
-            })
-    
-            const resData = await res.json()
-            console.log(resData)
-          
-          }
+            },
+            body: JSON.stringify(command)
+        })
 
-          if (submit) {
-            
-            const command = {"pass": pass, "msg": {"oled": msg.split(" ")}}
-              f(command)
-          }
-        }
-        check = false
-      }, [submit])
+        const resData = await res.json()
+        console.log(resData)
+        setRes(resData)
 
- const handleSubmit = (e) => {
-    e.preventDefault()
-    setSubmit(true)
- }
+        formRef.current.reset()
+    }
 
- const inputStyles = 'h-[3rem] bg-transparent text-white placeholder-white border border-white/10 p-2'
+    const inputStyles = 'h-[3rem] bg-transparent text-white placeholder-white border border-white/10 p-2'
 
-  return (
-    <div className='bg-slate-700 flex flex-col gap-4 text-white p-4 rounded-md shadow-lg w-[90%]'>
-        <p className='text-xlg font-bold'>Send Command to Pico W</p>
-        <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
-            <input type='password' placeholder='Enter Command password' onChange={(e) => {setPass(e.target.value)}} className={inputStyles}/>
-            <input type='text' placeholder='Enter One or Two Word Message' onChange={(e) => {setMsg(e.target.value)}} className={inputStyles}/>
-            <button type='submit' className='p-2 bg-slate-300 text-black'>Submit</button>
-        </form>
-    </div>
-  )
+    return (
+        <div className='bg-slate-700 flex flex-col gap-4 text-white p-4 rounded-md shadow-lg w-[90%]'>
+            <p className='text-xlg font-bold'>Send Command to Pico W</p>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-4 ' ref={formRef}>
+                <input type='password' placeholder='Enter Command password' onChange={(e) => { setPass(e.target.value) }} className={inputStyles} />
+                <input type='text' placeholder='Enter One or Two Word Message' onChange={(e) => { setMsg(e.target.value) }} className={inputStyles} />
+                <button type='submit' className='p-2 bg-slate-300 text-black'>Submit</button>
+            </form>
+            <p>{res ? res.msg === 'failed' ? "Failed" : "Message Sent" : ""}</p>
+        </div>
+    )
 }
 
 export default Command
